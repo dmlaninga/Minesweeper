d3e3c5873159d82610ca53f31c87a87d43552dde
Daniel Laninga <dmlaninga@gmail.com>
2018-04-20 20:34:52 -0700

    Refactored with Babel
    Used command line babel to refactor the es6 code. Game playable by running node game.js.

--- src/board.js
+++ src/board.js
@@ -1,26 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game Over');
-      this._board.print();
-    } else if (!this._board.hasSafeTile()) {
-      console.log('You Win!');
-    } else {
-      console.log('Current Board:');
-      this._board.print();
-    } {
-
-    }
-  }
-}
-
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -117,7 +95,3 @@ class Board {
   return board;
 }
 }
-
-
-const g = new Game(3, 3, 3);
-g.playMove(0, 0 );
